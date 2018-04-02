5065def9eb46f60a427f1aa25cef1341892b3c85
f4tp <mr_treadwell@live.co.uk>
2018-04-02 08:53:28 +0100

    Problem fixed, it is now connecting and returning data from the local db; TO DO: populate the listbox with teh contents of the list that holds records returned

--- FormUI/DataAccess.cs
+++ FormUI/DataAccess.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Dapper;
 
 
@@ -18,9 +19,19 @@ namespace FormUI
             //create a new connection to SQL server
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("SampleDB")))
             {
-               // var output = connection.Query<Person>($"SELECT * FROM tblPeople").ToList();
+                // var output = connection.Query<Person>($"SELECT * FROM tblPeople").ToList();
 
-                var output =  connection.Query<Person>($"SELECT * FROM tblPeople WHERE LastName = '{lastname}'").ToList();
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                var output =  connection.Query<Person>($"SELECT * FROM dbo.tblPeople WHERE LastName = '{lastname}';").ToList();
+          
+
+                //var output1 = connection.Query<Person>("SELECT name, database_id, create_date FROM sys.databases").ToList();
+
+              
 
                 return output;
             }
